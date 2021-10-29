1c0d9975dddc4ec112590d57d840090112173821
Sergey Datsyk <datsyk.serg@gmail.com>
2021-10-29 10:21:21 +0900

    Camera follow code

---
+++ Assets/Scripts/Camera/Follow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Camera
+{
+    public class Follow : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+    
+        private Vector3 _offset;
+
+        private void Awake()
+        {
+            _offset = transform.position - _target.position;
+        }
+
+        private void LateUpdate()
+        {
+            transform.position = Vector3.Lerp(transform.position, _target.position + _offset, 0.8f);
+        }
+    }
+}
