34a6b17103e1495f7ec62ff251ab4a253af64d6f
Sergey Datsyk <datsyk.serg@gmail.com>
2021-10-29 10:26:03 +0900

    GameMain script

---
+++ Assets/Scripts/GameMain.cs
@@ -0,0 +1,48 @@
+﻿using Enemy;
+using Input;
+using Player;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMain : MonoBehaviour
+{
+    [SerializeField] private PlayerHealth _player;
+    [SerializeField] private GameObject _gameOverWindow;
+    [SerializeField] private VariableJoystick _joystick;
+    [SerializeField] private Transform _camera;
+
+    private void Awake()
+    {
+        _player.GetComponent<PlayerMovement>().Initialize(new JoystickInput(_joystick), _camera);
+        
+        EnemyInitialize(_player);
+        UIInitialize(_player);
+    }
+
+    private void OnDisable()
+    {
+        _player.Death -= OnPlayerDeath;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    private void UIInitialize(PlayerHealth playerHealth)
+    {
+        playerHealth.Death += OnPlayerDeath;
+        _gameOverWindow.SetActive(false);
+    }
+
+    private void EnemyInitialize(PlayerHealth playerHealth)
+    {
+        foreach (AttackPlayer attackPlayer in FindObjectsOfType<AttackPlayer>()) 
+            attackPlayer.Initialize(playerHealth);
+    }
+
+    private void OnPlayerDeath()
+    {
+        _gameOverWindow.SetActive(true);
+    }
+}
